416731b3c1e027e455c7952710b305164924903e
AkashMAbrrar <madborakash48@gmail.com>
2024-12-08 22:46:20 +0600

    this commit for delte a campaign

--- src/components/TableCampaign.jsx
+++ src/components/TableCampaign.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const TableCampaign = ({ campaign }) => {
   const { _id, title, name, photo, email } = campaign;
+
+  const handleDelete = (_id) => {
+    // console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/campaigns/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            // console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div className="overflow-x-auto">
@@ -44,6 +75,11 @@ const TableCampaign = ({ campaign }) => {
                 className="btn btn-ghost btn-xs">
                 Update
               </Link>
+              <button
+                onClick={() => handleDelete(_id)}
+                className="btn btn-error btn-xs">
+                Delete
+              </button>
             </th>
           </tr>
         </tbody>
